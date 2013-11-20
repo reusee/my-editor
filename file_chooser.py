56e87d7408b6f5fd0ff4eff2fcee20509d0c99f1
reus <reusee@ymail.com>
2013-11-20 21:38:19 +0800

    file chooser

--- file_chooser.py
+++ file_chooser.py
@@ -1,22 +1,96 @@
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
+import os
 
 class FileChooser(QDialog):
   def __init__(self, parent):
     super(FileChooser, self).__init__(parent)
 
-    self.edit = QLineEdit(self)
-    self.edit.returnPressed.connect(self.accept)
-    self.foo = None
+    self.path = ''
+    self.head = ''
 
-  def accept(self):
-    self.foo = self.edit.text()
-    super(FileChooser, self).accept()
+    self.layout = QVBoxLayout()
+    self.setLayout(self.layout)
+    self.layout.setAlignment(Qt.AlignTop)
+
+    self.edit = QLineEdit(self)
+    self.layout.addWidget(self.edit)
+    self.edit.returnPressed.connect(self.editPressed)
+    self.edit.textChanged.connect(self.updateList)
+
+    self.model = QStringListModel()
+    self.view = QListView()
+    self.view.setEditTriggers(QAbstractItemView.NoEditTriggers)
+    self.view.setModel(self.model)
+    self.layout.addWidget(self.view)
+    self.view.installEventFilter(self)
+
+    parent.resized.connect(self.resized)
+
+  def exec_(self):
+    self.edit.clear()
+    self.edit.setFocus()
+    self.updateList('')
+    super(FileChooser, self).exec_()
+
+  def resized(self, ev):
+    self.resize(ev.size())
+
+  def editPressed(self):
+    self.path = self.model.data(self.view.currentIndex(), Qt.DisplayRole)
+    path = os.path.join(self.head, self.path)
+    if os.path.isfile(path):
+      self.accept()
+    else: # open directory
+      path += os.path.sep
+      self.edit.setText(path)
+      self.updateList(path)
 
   def reject(self):
-    self.foo = None
+    self.path = None
     super(FileChooser, self).reject()
 
   def choose(self):
     self.exec_()
-    return self.foo
+    if not self.head or not self.path: return ''
+    return os.path.join(self.head, self.path)
+
+  def updateList(self, text):
+    head, tail = os.path.split(os.path.expanduser(text))
+    if head == '':
+      head = os.path.abspath('.')
+    self.head = head
+    try:
+      self.model.setStringList(sorted([f
+          for f in os.listdir(head)
+          if self.fuzzyMatch(tail, f)]))
+      self.view.setStyleSheet('QLineEdit { background-color: white; }')
+    except FileNotFoundError:
+      self.model.setStringList([])
+      self.view.setStyleSheet('QLineEdit { background-color: red; }')
+    self.view.setCurrentIndex(self.model.index(0, 0))
+
+  def fuzzyMatch(self, key, s):
+    keyI = 0
+    sI = 0
+    while keyI < len(key) and sI < len(s):
+      if s[sI].lower() == key[keyI].lower():
+        sI += 1
+        keyI += 1
+      else:
+        sI += 1
+    return keyI == len(key)
+
+  def eventFilter(self, watched, event):
+    if event.type() == QEvent.KeyPress and event.key() == Qt.Key_Return:
+      path = self.model.data(self.view.currentIndex(), Qt.DisplayRole)
+      path = os.path.join(self.head, path)
+      if os.path.isfile(path):
+        self.path = path
+        self.accept()
+      else:
+        path += os.path.sep
+        self.edit.setText(path)
+        self.updateList(path)
+      return True
+    return False
